0061b0c9a632f9dfe13d180c1cba6f4adf5772d8
Sebastian Morr <sebastian@morr.cc>
2015-04-07 01:09:13 +0200

    Clean up stuff

--- content/index.md
+++ content/index.md
@@ -6,44 +6,16 @@ headless: true
 <div class="intro">
 <img src="/assets/images/avatar.png">
 
-<p>Hey there! My name is Sebastian Morr, I’m a computer science student from Germany. On this site I publish things I create and share stuff I like.</p>
+<p>Hey there! My name is Sebastian Morr, I’m studying computer science at TU Braunschweig, Germany. On this site I publish things I create and share stuff I like.</p>
 
 <p>Here's how to <a href="/about/">contact</a> me. And now, let's get started with the latest blog entries:</p>
 </div>
 
-<%
-categories = ["software", "documents", "art"]
-buckets = {}
-categories.each do |c|
-    buckets[c] = []
-end
-buckets["The Rest"] = []
-things.each do |t|
-    c = categories.find{|c| has_tag(t,c)}
-    if not c
-        c = "The Rest"
-    end
-    buckets[c] << t
-end
-
-categories << "The Rest"
-%>
-
-<% categories.each do |cat| %>
-<h1><%= cat %></h1>
-<div id="projects">
-<% buckets[cat].chronologic.reverse.each do |item| %>
-<%= render "project_box", {:item => item} %>
+<% categories.each do |name, items| %>
+<h1><%= name %></h1>
+<div class="boxes">
+<% items.each do |item| %>
+<%= render "box", {:item => item} %>
 <% end %>
 </div>
 <% end %>
-
-<!-- <% things[0..4].each do |item| %>
-<% render "article", {:item => item} do %>
-<%= item.compiled_content %>
-<% end %>
-<% end %>
-
-<div class="more">
-<a href="/blog/">← Previous entries</a>
-</div> -->
--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,34 +1,41 @@
 include Nanoc::Helpers::Rendering
 
+def categories
+    c = {}
+    c["Software"] = with_tag("software")
+    c["Art"] = with_tag("art")
+    c["Unsorted"] = things - c.values.flatten.uniq
+    c
+end
+
 def things
-    @items.select{|i| i[:published]}.chronologic.reverse
+    @items['/'].children.select{|i| i[:published]}.sort_by{|i| i[:published]}.reverse
 end
 
 def toplevel
-    @items['/'].children.select{|i| i[:title] and i[:order] and not i[:hidden]}.sort_by{|i| i[:order]}
+    @items['/'].children.select{|i| i[:order] }.sort_by{|i| i[:order]}
 end
 
 def link_to item
     "<a href=\"#{item.path}\">#{item[:title]}</a>"
 end
 
-def tags
-    @items.select{|i| i[:tags]}.map{|i| i[:tags]}.join(", ").split(",").map{|t| t.strip}.uniq
-end
-
-def tags_for item
+def tags_for item, link=true
     if item[:tags]
-        item[:tags].split(",").map do |tag|
-            tag = tag.strip
-            "<a href=\"/blog/tag/#{tag}/\">#{tag}</a>"
+        item[:tags].map do |tag|
+            if link
+                "<a href=\"/tag/#{tag}/\">#{tag}</a>"
+            else
+                tag
+            end
         end.join(", ")
     else
-        []
+        ""
     end
 end
 
 def lang_for item
-    if tags_for(item).include? "german"
+    if item[:tags].include? "german"
         "de"
     else
         "en"
@@ -62,38 +69,10 @@ def thumbnail_for item
     end
 end
 
-class Array
-    def chronologic
-        sort_by do |item|
-            item[:published] || Date.new
-        end
-    end
-    def enum
-        map { |item|
-            entry = ""
-            if item[:cover]
-                entry << "<img src=\"#{item.path}cover.png\">"
-            end
-            if item[:published]
-                entry << item[:published].strftime("%Y-%m-%d") << " -- "
-            end
-            entry << link_to(item)
-            if item[:tags]
-                entry << " / <span class=\"meta\">#{tags_for item}</span>"
-            end
-            entry
-        }.join("\n\n")
-    end
-end
-
 def with_tag tag
-    @items.select do |item|
-        item[:tags] and item[:tags].split(",").map{|t| t.strip}.include? tag
+    things.select do |item|
+        item[:tags] and item[:tags].include? tag
     end
-end
-
-def has_tag item, tag
-    item[:tags] and item[:tags].split(",").map{|t| t.strip}.include? tag
 end
 
 def subtitle
