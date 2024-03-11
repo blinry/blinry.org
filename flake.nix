68ea6a1ec6b15016bd0ef3fe6c15b3228b82db30
blinry <mail@blinry.org>
2024-03-11 14:38:29 +0100

    Remove flake-utils

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
 {
   inputs.nanoc-setup.url = "github:blinry/nanoc-setup";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { nanoc-setup, ... }: {
     inherit (nanoc-setup) devShells;
